d55eedc53f7476bb4eeaddbef04580595ed8bfc5
wonhee <wonhee010@gmail.com>
2021-01-09 22:18:14 +0900

    feat: add setup func

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpNavigationBar()
     }
-
+    
+    // MARK: - setup
+    private func setUpNavigationBar() {
+        let searchBar = UISearchController()
+        self.navigationItem.searchController = searchBar
+        self.navigationItem.hidesSearchBarWhenScrolling = true
+        
+        let groupNavigationButton = UIBarButtonItem(title: "그룹", style: .plain, target: self, action: nil)
+        self.navigationItem.leftBarButtonItem = groupNavigationButton
+    }
 
 }
 
