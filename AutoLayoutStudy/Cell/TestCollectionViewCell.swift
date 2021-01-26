c942cd0a85c937236c3c41baf5368f02b4751907
wonhee <wonhee010@gmail.com>
2021-01-26 22:49:57 +0900

    feat: add flowlayout

---
+++ AutoLayoutStudy/Cell/TestCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TestCollectionViewCell.swift
+//  AutoLayoutStudy
+//
+//  Created by Wonhee on 2021/01/26.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var testImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var collection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        collection.register(UINib(nibName: "TestCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+        collection.dataSource = self
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets.zero
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.minimumLineSpacing = 10
+        
+        let halfWidth = UIScreen.main.bounds.width / 3
+        flowLayout.itemSize = CGSize(width: halfWidth * 0.9 , height: halfWidth * 0.9)
+        self.collection.collectionViewLayout = flowLayout
     }
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 14
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? TestCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.testImage.image = UIImage(named: "test1")
+        return cell
+    }
+}
 
+extension ViewController: UICollectionViewDelegate {
+    
 }
 
