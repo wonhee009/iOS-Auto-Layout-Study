28574b5012481ab8d87af1ca2becd295f92a8dfd
wonhee <wonhee010@gmail.com>
2021-01-26 22:19:28 +0900

    feat: add test image

---
+++ AutoLayoutStudy/Cell/ImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCollectionViewCell.swift
+//  AutoLayoutStudy
+//
+//  Created by Wonhee on 2021/01/26.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
